878e642e1a03bca539f6c93ce413edfa41228dfe
Kadir Cem Yılmaz <kadircemyilmaz@ogrenci.beykoz.edu.tr>
2022-05-19 19:54:47 +0300

    Garage

---
+++ Go to Moon/Assets/garage_Script.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class garage_Script : MonoBehaviour
+{
+    [Header("Specs")]
+    public int speed;
+    public int durability;
+    public int duration;
+
+    [Header("Upgrades")]
+    public int rigidity;
+    public int engine;
+    public int wing;
+    public int accelerate;
+    public int fuel;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void RigidityUpgrade()
+    {
+
+
+    }
+}
